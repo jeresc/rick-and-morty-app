e652ff178e15b8aa3be80dd72a30aaaadc5e93b8
jeresc <jeremiastomassrc@gmail.com>
2023-06-29 13:32:03 -0300

    Fixing relative paths

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <Provider store={store}>
-    <BrowserRouter basename="">
+    <BrowserRouter basename="/rick-and-morty-app">
       <App />
     </BrowserRouter>
   </Provider>
